e4bb2d4d9f96481171e0d41e6fd9441e571778cf
Om Mishra <om.works01@gmail.com>
2024-11-23 20:52:11 +0530

    fixes-f

--- auth.ts
+++ auth.ts
@@ -2,14 +2,11 @@ import NextAuth, { type DefaultSession } from "next-auth";
 import { PrismaAdapter } from "@auth/prisma-adapter";
 import authConfig from "@/auth.config";
 import { db } from "@/lib/db";
-import { UserRole } from "@prisma/client";
 import { getUserById } from "@/data/user";
 
 declare module "next-auth" {
   interface Session {
-    user: {
-      role: UserRole;
-    } & DefaultSession["user"];
+    user: {} & DefaultSession["user"];
   }
 }
 
@@ -38,7 +35,6 @@ export const { auth, handlers, signIn, signOut } = NextAuth({
         session.user.id = token.sub;
       }
       if (token.role && session.user) {
-        session.user.role = token.role as UserRole;
       }
       return session;
     },
@@ -46,7 +42,7 @@ export const { auth, handlers, signIn, signOut } = NextAuth({
       if (!token.sub) return token;
       const existingUser = await getUserById(token.sub);
       if (!existingUser) return token;
-      token.role = existingUser.role;
+
       return token;
     },
   },
